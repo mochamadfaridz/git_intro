5854597e8d031002b4d5e62eaf7b0d146691766e
Faridz <mochamadfaridz@gmail.com>
2018-02-03 22:06:03 +0700

    perubahan index

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 		<title>i-Dev git | Home</title>
 		</head>
 		<body>
-		<h1>Hello Git</h1>
+		<h1>Hello Git Faridz</h1>
 		<table border="1">
 			<tr>
 				<th><a href="home.php">Home</a></th> <!-- isi base_url
 				<th><a href="about.php">About</a></th>
 			</tr>
 			<tr>
-				<td colspan+"2">Ini Home</td>
+				<td colspan+"2">wmwmwmmwmw</td>
 				</tr>
 				<tr>
 				<td colspan="2" Footer i-Dev &copy; 2018</td>
